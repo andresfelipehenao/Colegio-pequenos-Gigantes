6475550610fe46cbcf59978340e8ae3139612556
andresfelipehenao <39415907+andresfelipehenao@users.noreply.github.com>
2023-02-14 16:11:58 -0500

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,56 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light')
+const closeLight = document.querySelector('.close')
+
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'));
+    })
+});
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+        hamburguer.style.opacity = '1';
+    }
+})
+
+
+const aparecerImagen = (imagen)=>{
+    imagenLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+    hamburguer.style.opacity = '0';
+}
+
+/*
+const imagenes = document.querySelector('.img-galeria')
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light')
+const hamburguer1 = document.querySelector('.hamburguer')
+
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'));
+    })
+});
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+        hamburguer.style.opacity = '1';
+    }
+})
+
+
+const aparecerImagen = (imagen)=>{
+    imagenLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+    hamburguer1.style.opacity = '0';
+}*/
